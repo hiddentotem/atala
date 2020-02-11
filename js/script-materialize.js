cfe43e0235e6c42235901982fdd8706ee395c5c8
hiddentotem <60638836+hiddentotem@users.noreply.github.com>
2020-02-11 16:43:06 +0700

    First commit

---
+++ js/script-materialize.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({hover: true, coverTrigger: false, constrainWidth: false});
+    $('.collapsible').collapsible({accordion: true});
+    $('.slider').slider({indicators: false, height: 600});
+ 
+});
